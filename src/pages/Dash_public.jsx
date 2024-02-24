ee1625de83ae1e1b26a1fe3176d6af0aaadedf91
vti-network <155095106+vti-network@users.noreply.github.com>
2024-02-24 17:28:45 +0700

    Update Dash_public.jsx

--- src/pages/Dash_public.jsx
+++ src/pages/Dash_public.jsx
@@ -1,7 +1,17 @@
 //Dash_public.jsx
 import { Navbar } from '../com/Navbar.jsx';
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 export function DashboardPublic (){
+    const navigate = useNavigate();
+        useEffect(() => {
+        const checkToken = localStorage.getItem('refresh_token');
+        if (!checkToken) {
+            navigate("/blank");
+            alert("Please log in first");
+        }
+    }, [navigate]);
     return (
     <>
         <div className='container'>
